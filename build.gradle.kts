1376f5f4b6884a72f7d59880c95b58ced9131377
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2025-01-10 15:31:35 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.sonatype.central.portal.publisher)
+    `maven-publish`
+}
+
+val baseVersion = "0.0.1"
+val commitHash = System.getenv("COMMIT_HASH")
+val snapshotVersion = "${baseVersion}-dev.$commitHash"
+
+group = "app.simplecloud"
+version = if (commitHash != null) snapshotVersion else baseVersion
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(rootProject.libs.kotlin.jvm)
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "simplecloud"
+            url = uri("https://repo.simplecloud.app/snapshots/")
+            credentials {
+                username = System.getenv("SIMPLECLOUD_USERNAME")?: (project.findProperty("simplecloudUsername") as? String)
+                password = System.getenv("SIMPLECLOUD_PASSWORD")?: (project.findProperty("simplecloudPassword") as? String)
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
+
+signing {
+    if (commitHash != null) {
+        return@signing
+    }
+
+    sign(publishing.publications)
+    useGpgCmd()
+}
